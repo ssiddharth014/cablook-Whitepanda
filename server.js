da97c3affc9772e4d2b1caae4799d32d83faeb01
siddharth <ssiddharth014@gmail.com>
2020-05-13 09:40:00 +0530

    server2

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 const express= require('express');
 //const favicon= require('express-favicon');
 const path=require('path');
-const port=process.env.PORT || 8080;
+const port=process.env.PORT || 5000;
 const app= express();
 //app.use (favico(__dirname + '/build/favicon.ico'));
-app.use(express.static(__dirname));
 
 
 
+
+const publicpath= path.join(__dirname,'..','public');
+app.use(express.static(publicpath));
+
+
+//app.listen(port)
 
 app.use(express.static(path.join(__dirname,'build')));
-app.get('/ping',function(req,res){
-	return res.send('pong');
+
+app.get('*',function(req,res){
+	res.sendFile(path.join(publicpath,'index.html'));
 
 });
-app.get('/*',function(req,res){
-	res.sendFile(path.join(__dirname,'build','index.html'));
 
+app.listen(port,function(err,){
+
+    if (err){
+        console.log(`error in running the server : ${err}`);
+    }
+
+
+
+    console.log(`server is up and running at port :${port}`);
 });
-app.listen(port);
